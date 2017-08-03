18562bd55367f8f55dc830384260c248fcd25144
Will Rogers <wahrogers@gmail.com>
2017-08-03 09:06:07 +0100

    Invert the logic...

--- tbot/trump.py
+++ tbot/trump.py
@@ -16,8 +16,8 @@ NO = "No"
 class Tweeter(betfair.Tweeter):
 
     def __init__(self, api):
-        super(Tweeter, self).__init__(api, URL, MSG, YES)
-        self._max = 50.2
-        self._max_date = datetime.date(2017, 8, 2)
-        self._min = 49.9
-        self._min_date = datetime.date(2017, 7, 30)
+        super(Tweeter, self).__init__(api, URL, MSG, NO)
+        self._max = 50.1
+        self._max_date = datetime.date(2017, 7, 30)
+        self._min = 49.0
+        self._min_date = datetime.date(2017, 8, 2)
